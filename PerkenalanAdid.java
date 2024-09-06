ef3ec42c2a80c113bee693b48159f39920654c6f
Adidtiasmara <mfirmanadid@gmail.com>
2024-09-06 13:32:04 +0700

    perkenalan

---
+++ PerkenalanAdid.java
@@ -0,0 +1,21 @@
+public class PerkenalanAdid {
+    public static void main(String[] args) {
+       
+            System.out.println("-------------------------------------------------------------");
+            System.out.println("-- Data Mahasiswa Baru Jurusan Teknologi Informasi POLINEMA--");
+            System.out.println("-------------------------------------------------------------");
+            System.out.println("Nama    : Muhammad Firman Aditiasmara");
+            System.out.println("NIM     : 244107020094");
+            System.out.println("Kelas   : 1E");
+            System.out.println("No Absen: 18");
+            System.out.println("Prodi   : D-IV Teknik Informatika");
+            System.out.println("Alamat  : Desa Jatiguwi,Sumberoucung,Kabupaten Malang");
+            System.out.println("No HP   : 081252590903");
+            System.out.println("Hobi    : Berenang                                           ");
+    
+    
+    
+        }
+    }
+     
+    
